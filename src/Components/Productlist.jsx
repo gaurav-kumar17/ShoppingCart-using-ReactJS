3dfbcc9060cb86da9d3c5be60143cdaa7cd27bb4
gaurav-kumar17 <gk170106@gmail.com>
2022-10-17 19:53:40 +0530

    second commit

---
+++ src/Components/Productlist.jsx
@@ -0,0 +1,14 @@
+import "../App.css";
+const Productlist = ({ product }) => {
+  return <div>
+  {product.map((productItem, productIndex) => {
+
+    return(
+        <div>
+
+        </div>
+    )
+  })}
+  </div>;
+};
+export default Productlist;
