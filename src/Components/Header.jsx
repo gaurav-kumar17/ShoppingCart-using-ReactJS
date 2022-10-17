e7c3c456b2f73a255a7f0b7f3cbe50880eade84a
gaurav-kumar17 <gk170106@gmail.com>
2022-10-17 19:38:27 +0530

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import '../App.css'
+const Header = () => {
+
+  return (
+    <div className="flex shopping-card">
+      <div>Shopping Cart App</div>
+      <div>
+        Cart
+        <sup>{"2"}</sup>
+      </div>
+    </div>
+  );
+};
+export default Header;
