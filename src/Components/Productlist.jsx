a883083a8905b43ae89db868cdee56981187c17d
gaurav-kumar17 <gk170106@gmail.com>
2022-10-17 20:31:36 +0530

    third commit

--- src/Components/Productlist.jsx
+++ src/Components/Productlist.jsx
@@ -1,14 +1,26 @@
 import "../App.css";
+import React from "react";
 const Productlist = ({ product }) => {
-  return <div>
-  {product.map((productItem, productIndex) => {
+  console.log("hi",product[0].name);
+  return (
+    <div className="flex">
+      {
+        product.map((productItems, productIndex) => {
 
-    return(
-        <div>
+        return (
+            <div style={{width:'50%', border:"1px solid red"}}>
+                <div className="product-item">
+                    <img src={productItems.url} width="50%" />
+                    <p>{productItems.name} | {productItems.category}</p>
+                    <p>{productItems.seller}</p>
+                    <p>Rs. {productItems.price} /-</p>
+                </div>
+            </div>
+            )
 
-        </div>
-    )
-  })}
-  </div>;
+      })
+      }
+    </div>
+  );
 };
 export default Productlist;
